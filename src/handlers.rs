f54d8ededa461cfac45385c973a3c426efd69fc1
ryo <nakayaryo0716@gmail.com>
2023-12-24 02:44:27 +0900

    create app init

---
+++ src/handlers.rs
@@ -0,0 +1,36 @@
+use proconio::input;
+
+pub fn print_music_list() {
+    println!("
+0: 終了
+1: 窓辺のドロップス
+2: 星降る
+3: 寝落ち
+4: 露草
+5: 残暑
+    ");
+}
+
+pub fn music_path_handler(num: u32) -> (&'static str, u64) {
+    let music_path = match num {
+        1 => ("assets/drops.mp3", 135),
+        2 => ("assets/hoshihuru.mp3", 160),
+        3 => ("assets/neochi.mp3", 115),
+        4 => ("assets/rokusa.mp3", 155),
+        5 => ("assets/zansho.mp3", 170),
+        _ => {
+            panic!();
+        }
+    };
+
+    music_path
+}
+
+pub fn select_number() -> u32 {
+    println!("What music do you want?");
+    input! {
+        num: u32,
+    }
+
+    num
+}
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod handlers;
+
+use std::{io::BufReader, fs::File, thread};
+use rodio::{OutputStream, Decoder, Source};
+use handlers::{print_music_list, music_path_handler, select_number};
+
+fn main() {
+    print_music_list();
+    
+    loop{
+        let music_number = select_number();
+
+        if music_number == 0 {
+            println!("See you");
+            break;
+        }
+
+        // 選択された曲のファイルがあるパスと曲の時間を取得
+        let (selected_music_path, music_time) = music_path_handler(music_number);
+
+        // 物理サウンドデバイスの出力ストリームハンドルを取得
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+        let file = BufReader::new(File::open(selected_music_path).unwrap());
+        let source = Decoder::new(file).unwrap();
+
+        let music_thread = thread::spawn(move || {
+            let _ = stream_handle.play_raw(source.convert_samples());
+
+            println!("now playing...");
+            // 曲は別のオーディオスレッドで流れるようになっているため
+            // このスレットを曲の時間分だけスリープ
+            thread::sleep(std::time::Duration::from_secs(music_time));
+            println!("finish");
+        });
+
+        music_thread.join().unwrap();
+    }
+}
