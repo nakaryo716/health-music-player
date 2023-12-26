2a1e708b0024e6047d80f4f34706fe0f8415ef77
ryo <nakayaryo0716@gmail.com>
2023-12-26 13:21:20 +0900

    実行ファイルから実行できるようにpathを実行ファイルからの相対パスに変更

--- src/handlers.rs
+++ src/handlers.rs
@@ -13,11 +13,11 @@ pub fn print_music_list() {
 
 pub fn music_path_handler(num: u32) -> (&'static str, u64) {
     match num {
-        1 => ("assets/drops.mp3", 135),
-        2 => ("assets/hoshihuru.mp3", 160),
-        3 => ("assets/neochi.mp3", 115),
-        4 => ("assets/rokusa.mp3", 155),
-        5 => ("assets/zansho.mp3", 170),
+        1 => ("../../assets/drops.mp3", 135),
+        2 => ("../../assets/hoshihuru.mp3", 160),
+        3 => ("../../assets/neochi.mp3", 115),
+        4 => ("../../assets/rokusa.mp3", 155),
+        5 => ("../../assets/zansho.mp3", 170),
         _ => {
             panic!();
         }
